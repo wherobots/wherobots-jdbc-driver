7b8ede3eb2d2f821bbbf0490cd45aa998bf1644a
Maxime Petazzoni <max@wherobots.com>
2024-04-26 14:30:36 -0700

    feat: boilerplate for Driver and Connection classes

--- lib/src/main/java/com/wherobots/db/jdbc/WherobotsJdbcDriver.java
+++ lib/src/main/java/com/wherobots/db/jdbc/WherobotsJdbcDriver.java
@@ -1,6 +1,53 @@
 package com.wherobots.db.jdbc;
 
+import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class WherobotsJdbcDriver implements Driver {
+
+    private static final Logger logger = Logger.getLogger(WherobotsJdbcDriver.class.getName());
+
+    private static final Logger PARENT_LOGGER = Logger.getLogger(WherobotsJdbcDriver.class.getPackageName());
+    private static final int MAJOR_VERSION = 1;
+    private static final int MINOR_VERSION = 0;
+
+    @Override
+    public Connection connect(String url, Properties info) throws SQLException {
+        return new WherobotsJdbcConnection();
+    }
+
+    @Override
+    public boolean acceptsURL(String url) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) {
+        return new DriverPropertyInfo[0];
+    }
+
+    @Override
+    public int getMajorVersion() {
+        return MAJOR_VERSION;
+    }
+
+    @Override
+    public int getMinorVersion() {
+        return MINOR_VERSION;
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        // TODO: Run JDBC compliance test and evaluate.
+        return false;
+    }
+
+    @Override
+    public Logger getParentLogger() {
+        return PARENT_LOGGER;
+    }
 }
