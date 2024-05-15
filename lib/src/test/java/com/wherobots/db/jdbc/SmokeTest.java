d180b4913c32e36852b4a86d6c8bdf03c8e524c2
Maxime Petazzoni <max@wherobots.com>
2024-05-14 21:24:03 -0700

    feat: implement simple smoke test

--- lib/src/test/java/com/wherobots/db/jdbc/SmokeTest.java
+++ lib/src/test/java/com/wherobots/db/jdbc/SmokeTest.java
@@ -3,12 +3,43 @@ package com.wherobots.db.jdbc;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class SmokeTest {
     public static void main(String[] args) throws Exception {
         DriverManager.setLogWriter(new PrintWriter(System.out));
         DriverManager.registerDriver(new WherobotsJdbcDriver());
-        Connection conn = DriverManager.getConnection("jdbc:wherobots://cloud.wherobots.com", new Properties());
+
+        String sql = """
+            SELECT
+                id,
+                names['primary'],
+                geometry,
+                population
+            FROM
+                wherobots_open_data.overture_2024_02_15.admins_locality
+            WHERE localityType = 'country'
+            SORT BY population DESC
+            LIMIT 10
+        """;
+
+        Properties props = new Properties();
+        props.put(WherobotsJdbcDriver.API_KEY_PROP, args[0]);
+
+        try (Connection conn = DriverManager.getConnection("jdbc:wherobots://api.staging.wherobots.com", props)) {
+            Statement stmt = conn.createStatement();
+            try (ResultSet result = stmt.executeQuery(sql)) {
+                while (result.next()) {
+                    System.out.printf("%s %s %s %d%n",
+                            result.getString(1),
+                            result.getString(2),
+                            result.getString(3),
+                            result.getInt(4)
+                    );
+                }
+            }
+        }
     }
 }
