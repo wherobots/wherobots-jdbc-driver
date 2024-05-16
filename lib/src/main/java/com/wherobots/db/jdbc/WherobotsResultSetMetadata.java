7726ced5e27fe579d267e119cf803170c2cd85ea
Maxime Petazzoni <max@wherobots.com>
2024-05-15 22:25:26 -0700

    feat: skeleton implementation of ResultSetMetadata

---
+++ lib/src/main/java/com/wherobots/db/jdbc/WherobotsResultSetMetadata.java
@@ -0,0 +1,140 @@
+package com.wherobots.db.jdbc;
+
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+public class WherobotsResultSetMetadata implements ResultSetMetaData {
+
+    private static final Logger logger = LoggerFactory.getLogger(WherobotsResultSetMetadata.class);
+
+    private final Schema schema;
+    private final String[] fields;
+
+    public WherobotsResultSetMetadata(Schema schema) {
+        this.schema = schema;
+        this.fields = schema.getFields().stream().map(Field::getName).toArray(String[]::new);
+        logger.info("ResultSet({})", Arrays.asList(fields));
+    }
+
+    @Override
+    public int getColumnCount() {
+        return schema.getFields().size();
+    }
+
+    @Override
+    public boolean isAutoIncrement(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isCaseSensitive(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isSearchable(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isCurrency(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int isNullable(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public boolean isSigned(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public int getColumnDisplaySize(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getColumnLabel(int column) throws SQLException {
+        return getColumnName(column);
+    }
+
+    @Override
+    public String getColumnName(int column) throws SQLException {
+        return this.fields[column - 1];
+    }
+
+    @Override
+    public String getSchemaName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public int getPrecision(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int getScale(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getTableName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public String getCatalogName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public int getColumnType(int column) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public String getColumnTypeName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public boolean isReadOnly(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isWritable(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean isDefinitelyWritable(int column) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public String getColumnClassName(int column) throws SQLException {
+        return "";
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+}
